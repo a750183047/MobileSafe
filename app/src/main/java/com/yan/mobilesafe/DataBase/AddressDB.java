5a8c91f6a5f6055372a0ee756721c5fc2f5c52fc
a750183047 <a750183047@gmail.com>
2015-11-30 16:20:54 +0800

    号码归属地 正则匹配

--- app/src/main/java/com/yan/mobilesafe/DataBase/AddressDB.java
+++ app/src/main/java/com/yan/mobilesafe/DataBase/AddressDB.java
@@ -1,5 +1,6 @@
 package com.yan.mobilesafe.DataBase;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
@@ -15,6 +16,22 @@ public class AddressDB {
         //获取数据库对象
         SQLiteDatabase database = SQLiteDatabase.openDatabase(PATH,null,SQLiteDatabase.OPEN_READONLY);
 
-        return null;
+        //手机号正则表达式匹配
+        //1 + （3.4.5.6.7.8)+(9位数字)
+        //^1[3-8]\d{9}$
+
+        if (number.matches("^1[3-8]\\d{9}$")){
+            Cursor cursor = database.rawQuery("select location from data2 where id = (select outkey from data1 where id = ?)"
+            ,new String[]{number.substring(0,7)});
+            if (cursor.moveToNext()){
+                address = cursor.getString(0);
+            }
+            cursor.close();
+        }else if (number.matches("^\\d+$")){ //匹配数字
+
+        }
+
+        database.close();
+        return address;
     }
 }
--- app/src/main/java/com/yan/mobilesafe/activity/Atool/AddressActivity.java
+++ app/src/main/java/com/yan/mobilesafe/activity/Atool/AddressActivity.java
@@ -3,7 +3,13 @@ package com.yan.mobilesafe.activity.Atool;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.yan.mobilesafe.DataBase.AddressDB;
 import com.yan.mobilesafe.R;
 
 /**
@@ -12,13 +18,29 @@ import com.yan.mobilesafe.R;
  */
 public class AddressActivity extends AppCompatActivity {
 
+    private EditText inputNumber;
+    private Button select;
+    private TextView result;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_address_layout);
 
+        inputNumber = (EditText) findViewById(R.id.et_number_address);
+        select = (Button) findViewById(R.id.btn_ok);
+        result = (TextView) findViewById(R.id.tv_result);
 
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(inputNumber.getText().toString())){
+                    result.setText(AddressDB.getAddress(inputNumber.getText().toString()));
+                }
+            }
+        });
 
     }
 }
